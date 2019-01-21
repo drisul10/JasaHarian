206be408da800222d90c5cbe550cf58d24afaed6
Andrisoel <andrisoelto@gmail.com>
2019-01-21 16:40:20 +0700

    Create UI forgot password

---
+++ app/src/main/java/com/jasaharian/views/auth/forgotPassword/ForgotPasswordUI.kt
@@ -0,0 +1,33 @@
+package com.jasaharian.views.auth.forgotPassword
+
+import android.view.Gravity
+import android.widget.LinearLayout
+import com.jasaharian.R
+import org.jetbrains.anko.*
+
+class ForgotPasswordUI : AnkoComponent<ForgotPasswordActivity> {
+    override fun createView(ui: AnkoContext<ForgotPasswordActivity>) = with(ui) {
+        relativeLayout {
+            lparams(width = matchParent, height = matchParent) {
+                gravity = Gravity.CENTER_VERTICAL
+            }
+
+            linearLayout {
+                lparams(width = matchParent, height = wrapContent) {
+                    orientation = LinearLayout.VERTICAL
+                    margin = dip(25)
+                }
+
+                editText {
+                    id = R.id.et_email
+                    hintResource = R.string.hint_email
+                }
+
+                button {
+                    id = R.id.bt_send
+                    textResource = R.string.bt_send
+                }
+            }
+        }
+    }
+}
