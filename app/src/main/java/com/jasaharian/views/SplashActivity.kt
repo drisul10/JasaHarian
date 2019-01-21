1c140135c6a980112f2072fe88330d7dc26f0ca0
Andrisoel <andrisoelto@gmail.com>
2019-01-21 16:41:03 +0700

    Update small code

--- app/src/main/java/com/jasaharian/views/SplashActivity.kt
+++ app/src/main/java/com/jasaharian/views/SplashActivity.kt
@@ -3,7 +3,7 @@ package com.jasaharian.views
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.jasaharian.views.home.HomeActivity
-import com.jasaharian.views.auth.signIn.SignInActivity
+import com.jasaharian.views.auth.login.LoginActivity
 import org.jetbrains.anko.startActivity
 
 class SplashActivity : AppCompatActivity() {
@@ -13,7 +13,7 @@ class SplashActivity : AppCompatActivity() {
         if (1 > 2) {
             startActivity<HomeActivity>()
         } else {
-            startActivity<SignInActivity>()
+            startActivity<LoginActivity>()
         }
 
         finish()
