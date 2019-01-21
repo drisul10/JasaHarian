575ac4800557215d024e3c1124528d06b0b5810b
Andrisoel <andrisoelto@gmail.com>
2019-01-21 17:14:48 +0700

    Adding drawer layout

--- app/src/main/java/com/jasaharian/views/home/HomeUI.kt
+++ app/src/main/java/com/jasaharian/views/home/HomeUI.kt
@@ -1,7 +1,11 @@
 package com.jasaharian.views.home
 
+import android.support.v4.view.GravityCompat
+import com.jasaharian.R
 import org.jetbrains.anko.*
+import org.jetbrains.anko.design.coordinatorLayout
 import org.jetbrains.anko.design.navigationView
+import org.jetbrains.anko.support.v4._DrawerLayout
 import org.jetbrains.anko.support.v4.drawerLayout
 
 class HomeUI : AnkoComponent<HomeActivity> {
@@ -9,22 +13,38 @@ class HomeUI : AnkoComponent<HomeActivity> {
         relativeLayout {
             lparams(width = matchParent, height = matchParent)
 
-            textView {
-                text = "ini home"
-            }
-
             drawerLayout {
                 lparams(width = matchParent, height = matchParent) {
+                    id = R.id.ly_drawer
                     fitsSystemWindows = true
                 }
 
-                navigationView {
-
-                }.lparams(width = wrapContent, height = matchParent) {
-                    fitsSystemWindows = true
-                }
+                createAppBar(ui)
+                createNavigationView(ui)
             }
         }
     }
 
+    fun _DrawerLayout.createAppBar(ui: AnkoContext<HomeActivity>) {
+        coordinatorLayout {
+            lparams(width = matchParent, height = matchParent) {
+                fitsSystemWindows = false
+            }
+
+            //TODO: AppbarLayout & include
+        }
+    }
+
+    fun _DrawerLayout.createNavigationView(ui: AnkoContext<HomeActivity>) {
+        navigationView {
+            lparams(height = matchParent, gravity = GravityCompat.START) {
+                id = R.id.vw_nav
+                fitsSystemWindows = true
+            }
+
+            //TODO: setNavigationItemSelectedListener(ui.owner)
+            //      inflateHeaderView(R.layout.nav_header_main)
+            //      inflateMenu(R.menu.activity_main_drawer)
+        }
+    }
 }
