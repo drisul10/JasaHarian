115906274366c4e354bf10f1ee6c0987cf052f24
Andrisoel <andrisoelto@gmail.com>
2019-01-22 14:03:35 +0700

    Fixing navigation drawer

--- app/src/main/java/com/jasaharian/views/home/HomeActivity.kt
+++ app/src/main/java/com/jasaharian/views/home/HomeActivity.kt
@@ -2,17 +2,21 @@ package com.jasaharian.views.home
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.widget.Toolbar
-import android.view.Menu
 import android.view.MenuItem
 import com.jasaharian.R
 import org.jetbrains.anko.setContentView
+import org.jetbrains.anko.toast
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var toolbar: Toolbar
+    private lateinit var vwNavigation: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,22 +26,36 @@ class HomeActivity : AppCompatActivity() {
         toolbar = findViewById(R.id.toolbar)
 
         setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
 
-        //TODO: some code
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        toggle.syncState()
+
+        vwNavigation = findViewById(R.id.vw_nav)
+        vwNavigation.setNavigationItemSelectedListener(this)
     }
 
     override fun onBackPressed() {
-        super.onBackPressed()
-        //TODO: onBackPressed
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        return super.onCreateOptionsMenu(menu)
-        //TODO: onCreateOptionsMenu
-    }
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+        when (menuItem.itemId) {
+            R.id.nav_camera -> toast("Click Camera")
+            R.id.nav_gallery -> toast("Click Gallery")
+            R.id.nav_slideshow -> toast("Click Slideshow")
+            R.id.nav_manage -> toast("Click Manage")
+            R.id.nav_share -> toast("Click Share")
+            R.id.nav_send -> toast("Click Send")
+        }
 
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        return super.onOptionsItemSelected(item)
-        //TODO: onOptionsItemSelected
+        drawerLayout.closeDrawer(GravityCompat.START)
+
+        return true
     }
 }
