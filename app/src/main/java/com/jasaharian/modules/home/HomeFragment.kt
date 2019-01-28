ad487b91ef6f43fe0cd51398193345a83879a6cf
Andrisoel <andrisoelto@gmail.com>
2019-01-28 17:44:58 +0700

    New fragment

---
+++ app/src/main/java/com/jasaharian/modules/home/HomeFragment.kt
@@ -0,0 +1,16 @@
+package com.jasaharian.modules.home
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.jetbrains.anko.AnkoContext
+
+class HomeFragment : Fragment() {
+
+    private val ui = HomeUI<Fragment>()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+        ui.createView(AnkoContext.create(requireContext(), this))
+}
