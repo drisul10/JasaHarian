da065a32a24a37df562e9198ecb0e3da9d51d507
Andrisoel <andrisoelto@gmail.com>
2019-01-28 17:42:47 +0700

    Add some code

--- app/src/main/java/com/jasaharian/modules/main/MainUI.kt
+++ app/src/main/java/com/jasaharian/modules/main/MainUI.kt
@@ -1,9 +1,14 @@
-package com.jasaharian.views.home
+package com.jasaharian.modules.main
 
 import android.content.res.ColorStateList
 import android.graphics.Color
+import android.support.design.widget.AppBarLayout.LayoutParams.SCROLL_FLAG_ENTER_ALWAYS_COLLAPSED
+import android.support.design.widget.BottomNavigationView
+import android.support.design.widget.NavigationView
 import android.support.v4.content.ContextCompat
 import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.widget.Toolbar
 import com.jasaharian.R
 import org.jetbrains.anko.*
 import org.jetbrains.anko.design.appBarLayout
@@ -13,19 +18,25 @@ import org.jetbrains.anko.support.v4._DrawerLayout
 import org.jetbrains.anko.support.v4.drawerLayout
 import org.jetbrains.anko.appcompat.v7.toolbar
 import org.jetbrains.anko.design.bottomNavigationView
+import org.jetbrains.anko.support.v4.nestedScrollView
 
-class HomeUI : AnkoComponent<HomeActivity> {
-    override fun createView(ui: AnkoContext<HomeActivity>) = with(ui) {
+class MainUI : AnkoComponent<MainActivity> {
+
+    lateinit var toolbar: Toolbar
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navigationView: NavigationView
+    lateinit var bottomNavigationView: BottomNavigationView
+
+    override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
         relativeLayout {
             lparams(width = matchParent, height = matchParent) {
                 backgroundColorResource = R.color.colorBackground
             }
 
-            drawerLayout {
+            drawerLayout = drawerLayout {
                 lparams(width = matchParent, height = matchParent) {
                     id = R.id.ly_drawer
                     fitsSystemWindows = true
-                    z = 1f
                 }
 
                 createAppBar()
@@ -35,10 +46,9 @@ class HomeUI : AnkoComponent<HomeActivity> {
             relativeLayout {
                 lparams(width = matchParent, height = wrapContent) {
                     alignParentBottom()
-                    z = 0f
                 }
 
-                bottomNavigationView {
+                bottomNavigationView = bottomNavigationView {
                     lparams(width = matchParent, height = wrapContent)
 
                     id = R.id.vw_nav_bottom
@@ -62,18 +72,31 @@ class HomeUI : AnkoComponent<HomeActivity> {
             appBarLayout {
                 lparams(width = matchParent, height = wrapContent)
 
-                toolbar {
+                toolbar = toolbar {
                     lparams(width = matchParent, height = matchParent) {
                         id = R.id.toolbar
                         backgroundColor = Color.WHITE
                     }
+                    setTitleTextColor(Color.BLACK)
+                }.lparams {
+                    width = matchParent
+                    scrollFlags = SCROLL_FLAG_ENTER_ALWAYS_COLLAPSED
+                }
+            }
+
+            nestedScrollView {
+                lparams(width = matchParent, height = matchParent)
+
+                frameLayout {
+                    lparams(width = matchParent, height = wrapContent)
+                    id = R.id.fl_main_content
                 }
             }
         }
     }
 
-    private fun _DrawerLayout.createNavigationView(ui: AnkoContext<HomeActivity>) {
-        navigationView {
+    private fun _DrawerLayout.createNavigationView(ui: AnkoContext<MainActivity>) {
+        navigationView = navigationView {
             lparams(width = matchParent, height = matchParent, gravity = GravityCompat.START) {
                 id = R.id.vw_nav
                 fitsSystemWindows = true
