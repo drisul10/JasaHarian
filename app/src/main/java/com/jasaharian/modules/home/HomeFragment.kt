27dbcd56c8ddc0deb02d80b89988a9e1650eb05e
Andrisoel <andrisoelto@gmail.com>
2019-01-30 18:06:11 +0700

    Implementation view and presenter

--- app/src/main/java/com/jasaharian/modules/home/HomeFragment.kt
+++ app/src/main/java/com/jasaharian/modules/home/HomeFragment.kt
@@ -2,37 +2,78 @@ package com.jasaharian.modules.home
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.GridLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.jasaharian.R
 import com.jasaharian.models.HomeMenuFavorite
+import com.jasaharian.models.HomeMenuPromo
+import com.jasaharian.utils.gone
+import com.jasaharian.utils.visible
 import org.jetbrains.anko.AnkoContext
 import org.jetbrains.anko.support.v4.find
 
-class HomeFragment : Fragment() {
-
+class HomeFragment : Fragment(), HomeView {
+    private lateinit var presenter: HomePresenter
     private val ui = HomeUI<Fragment>()
-    private lateinit var rvListMenuFavorite: RecyclerView
+    private lateinit var adapterMenuFavorite: MenuFavoriteAdapter
+    private lateinit var adapterMenuPromo: MenuPromoAdapter
+    private var listMenuFavorite: ArrayList<HomeMenuFavorite> = arrayListOf()
+    private var listMenuPromo: ArrayList<HomeMenuPromo> = arrayListOf()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-        ui.createView(AnkoContext.create(requireContext(), this))
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+        listMenuFavorite.add(HomeMenuFavorite("Laundry", R.drawable.ic_code_white_24dp))
+        adapterMenuFavorite = MenuFavoriteAdapter(listMenuFavorite)
 
-        val list: ArrayList<HomeMenuFavorite> = arrayListOf()
-        list.add(HomeMenuFavorite("1", R.drawable.ic_account_circle_black_24dp))
-        list.add(HomeMenuFavorite("3", R.drawable.ic_account_circle_black_24dp))
-        list.add(HomeMenuFavorite("4", R.drawable.ic_account_circle_black_24dp))
-        list.add(HomeMenuFavorite("5", R.drawable.ic_account_circle_black_24dp))
-        list.add(HomeMenuFavorite("6", R.drawable.ic_account_circle_black_24dp))
-        list.add(HomeMenuFavorite("7", R.drawable.ic_email_black_24dp))
-        list.add(HomeMenuFavorite("8", R.drawable.ic_account_circle_black_24dp))
-        list.add(HomeMenuFavorite("9", R.drawable.ic_account_circle_black_24dp))
+        listMenuPromo.add(HomeMenuPromo(R.drawable.enambelas_sembilan))
+        adapterMenuPromo = MenuPromoAdapter(listMenuPromo)
+    }
 
-        rvListMenuFavorite = find(R.id.rv_list_menu_favorite)
-        rvListMenuFavorite.adapter = MenuFavoriteAdapter(list)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?):
+            View? = ui.createView(AnkoContext.create(requireContext(), this))
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        presenter = HomePresenter(this)
+        presenter.getMenuFavorite()
+        presenter.getMenuPromo()
+
+        ui.rvListMenuFavorite = find(R.id.rv_list_menu_favorite)
+        ui.rvListMenuFavorite.layoutManager = GridLayoutManager(context, 4)
+        ui.rvListMenuFavorite.adapter = MenuFavoriteAdapter(listMenuFavorite)
+        adapterMenuFavorite.notifyDataSetChanged()
+
+        ui.rvListMenuPromo = find(R.id.rv_list_menu_promo)
+        adapterMenuPromo = MenuPromoAdapter(listMenuPromo)
+        ui.rvListMenuPromo.adapter = adapterMenuPromo
+        adapterMenuPromo.notifyDataSetChanged()
+    }
+
+    override fun showMenuFavorite() {
+        //TODO: Isi data dari API
+    }
+
+    override fun showMenuPromo() {
+        //TODO: Isi data dari API
+    }
+
+    override fun showLoadingMenuFavorite() {
+        ui.progressbarMenuFavorite.visible()
+    }
+
+    override fun hideLoadingMenuFavorite() {
+        ui.progressbarMenuFavorite.gone()
+    }
+
+    override fun showLoadingMenuPromo() {
+        ui.progressbarMenuPromo.visible()
+    }
+
+    override fun hideLoadingMenuPromo() {
+        ui.progressbarMenuPromo.gone()
     }
 }
