f5b907d67cb637511bf3468c8e44ea5fcdd1dc94
Andrisoel <andrisoelto@gmail.com>
2019-01-30 18:41:42 +0700

    Delete NavigationDrawer

--- app/src/main/java/com/jasaharian/modules/main/MainActivity.kt
+++ app/src/main/java/com/jasaharian/modules/main/MainActivity.kt
@@ -1,12 +1,7 @@
 package com.jasaharian.modules.main
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.design.widget.NavigationView
-import android.support.v4.view.GravityCompat
-import android.support.v7.app.ActionBarDrawerToggle
-import android.view.MenuItem
-import android.view.View
+import android.support.v7.app.AppCompatActivity
 import com.jasaharian.R
 import com.jasaharian.modules.account.AccountFragment
 import com.jasaharian.modules.cart.CartFragment
@@ -14,44 +9,13 @@ import com.jasaharian.modules.home.HomeFragment
 import com.jasaharian.modules.inbox.InboxFragment
 import com.jasaharian.modules.transaction.TransactionFragment
 import org.jetbrains.anko.setContentView
-import org.jetbrains.anko.toast
 
-
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
-
-    //TODO: Capitalize
-
+class MainActivity : AppCompatActivity() {
     private val ui = MainUI()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ui.setContentView(this)
-
-        setSupportActionBar(ui.toolbar)
-        supportActionBar?.setDisplayShowTitleEnabled(true)
-
-        val toggle: ActionBarDrawerToggle = object : ActionBarDrawerToggle(
-            this,
-            ui.drawerLayout,
-            ui.toolbar,
-            R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close
-        ) {
-            override fun onDrawerOpened(drawerView: View) {
-                super.onDrawerOpened(drawerView)
-                ui.drawerLayout.z = 1f
-            }
-
-            override fun onDrawerClosed(drawerView: View) {
-                super.onDrawerClosed(drawerView)
-                ui.drawerLayout.z = 0f
-            }
-        }
-
-        ui.drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        ui.navigationView.setNavigationItemSelectedListener(this)
 
         ui.bottomNavigationView.setOnNavigationItemSelectedListener {
             when (it.itemId) {
@@ -65,6 +29,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                         .commit()
                     true
                 }
+
                 R.id.nav_menu_inbox -> {
                     supportFragmentManager
                         .beginTransaction()
@@ -72,6 +37,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                         .commit()
                     true
                 }
+
                 R.id.nav_menu_transaction -> {
                     supportFragmentManager
                         .beginTransaction()
@@ -79,6 +45,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                         .commit()
                     true
                 }
+
                 R.id.nav_menu_cart -> {
                     supportFragmentManager
                         .beginTransaction()
@@ -86,6 +53,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                         .commit()
                     true
                 }
+
                 R.id.nav_menu_account -> {
                     supportFragmentManager
                         .beginTransaction()
@@ -98,28 +66,5 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
 
         ui.bottomNavigationView.selectedItemId = R.id.nav_menu_home
-    }
-
-    override fun onBackPressed() {
-        if (ui.drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            ui.drawerLayout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
-    }
-
-    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
-        when (menuItem.itemId) {
-            R.id.i_menu_payment -> toast("menu payment")
-            R.id.i_menu_history -> toast("menu history")
-            R.id.i_menu_saved_place -> toast("menu saved place")
-            R.id.i_menu_help -> toast("menu help")
-            R.id.i_menu_emergency -> toast("menu emergency")
-            R.id.i_menu_setting -> toast("menu setting")
-        }
-
-        ui.drawerLayout.closeDrawer(GravityCompat.START)
-
-        return true
     }
 }
