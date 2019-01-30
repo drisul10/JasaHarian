3d31e0f583b4260106c62ba728201e7e119d7475
Andrisoel <andrisoelto@gmail.com>
2019-01-30 18:45:24 +0700

    Implementation onclick item recycler

--- app/src/main/java/com/jasaharian/modules/home/MenuFavoriteAdapter.kt
+++ app/src/main/java/com/jasaharian/modules/home/MenuFavoriteAdapter.kt
@@ -11,10 +11,14 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import com.jasaharian.R
 import com.jasaharian.models.HomeMenuFavorite
+import com.jasaharian.modules.laundry.LaundryActivity
 import org.jetbrains.anko.*
 import org.jetbrains.anko.cardview.v7.cardView
+import org.jetbrains.anko.sdk27.coroutines.onClick
 
-class MenuFavoriteAdapter(var list: ArrayList<HomeMenuFavorite> = arrayListOf()) : RecyclerView.Adapter<MenuFavoriteAdapter.MenuFavoriteViewHolder>() {
+class MenuFavoriteAdapter(
+    private var list: ArrayList<HomeMenuFavorite> = arrayListOf()
+) : RecyclerView.Adapter<MenuFavoriteAdapter.MenuFavoriteViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuFavoriteViewHolder {
         return MenuFavoriteViewHolder(MenuFavoriteUI().createView(AnkoContext.create(parent.context, parent)))
@@ -24,23 +28,33 @@ class MenuFavoriteAdapter(var list: ArrayList<HomeMenuFavorite> = arrayListOf())
         val itemMenuFavorite = list[position]
         holder.tvText.text = itemMenuFavorite.text
         holder.ivIcon.setImageResource(itemMenuFavorite.icon)
+
+        holder.glParent.onClick {
+            holder.loadActivity()
+        }
     }
 
-    override fun getItemCount(): Int {
-        return list.size
-    }
+    override fun getItemCount(): Int = list.size
 
     inner class MenuFavoriteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var tvText: TextView = itemView.findViewById(R.id.tv_text_menu_favorite)
-        var ivIcon: ImageView = itemView.findViewById(R.id.iv_icon_menu_favorite)
+        var tvText: TextView = itemView.find(R.id.tv_text_menu_favorite)
+        var ivIcon: ImageView = itemView.find(R.id.iv_icon_menu_favorite)
+        var glParent: GridLayout = itemView.find(R.id.gl_list_menu_favorite)
+
+        fun loadActivity() {
+            //TODO: Load activity sesuai menu
+            itemView.context.startActivity<LaundryActivity>()
+        }
     }
 
-    class MenuFavoriteUI : AnkoComponent<ViewGroup> {
+    private inner class MenuFavoriteUI : AnkoComponent<ViewGroup> {
         override fun createView(ui: AnkoContext<ViewGroup>): View {
             return with(ui) {
                 gridLayout {
+                    id = R.id.gl_list_menu_favorite
                     isColumnOrderPreserved = false
                     alignmentMode = GridLayout.ALIGN_BOUNDS
+                    bottomPadding = dip(15)
 
                     linearLayout {
                         lparams(width = matchParent, height = wrapContent)
@@ -61,7 +75,7 @@ class MenuFavoriteAdapter(var list: ArrayList<HomeMenuFavorite> = arrayListOf())
 
                                 imageView {
                                     id = R.id.iv_icon_menu_favorite
-                                    backgroundResource = R.drawable.circle_bg_purple
+                                    backgroundResource = R.drawable.bg_circle
                                 }.lparams(width = dip(35), height = dip(35))
 
                                 textView {
