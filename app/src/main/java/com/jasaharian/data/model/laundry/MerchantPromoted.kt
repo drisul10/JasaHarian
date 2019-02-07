36f0175d5b26fb0024cb33af2c10e82e178a0eed
Andrisoel <andrisoelto@gmail.com>
2019-02-07 15:41:22 +0700

    New model

---
+++ app/src/main/java/com/jasaharian/data/model/laundry/MerchantPromoted.kt
@@ -0,0 +1,7 @@
+package com.jasaharian.data.model.laundry
+
+data class MerchantPromoted(
+    var id: String,
+    var name: String,
+    var badge: Int
+)
---
+++ app/src/main/java/com/jasaharian/data/model/laundry/MerchantTopRating.kt
@@ -0,0 +1,7 @@
+package com.jasaharian.data.model.laundry
+
+data class MerchantTopRating(
+    var id: String,
+    var name: String,
+    var badge: Int
+)
